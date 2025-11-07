1f41a49e01f97582787eaf40f0d660e605bbb993
ldelanier <ldelanier@cikaba.com>
2025-11-07 10:41:10 +0100

    init

---
+++ src/main/java/com/example/apisecurite/ApiSecuriteApplication.java
@@ -0,0 +1,13 @@
+package com.example.apisecurite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSecuriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSecuriteApplication.class, args);
+    }
+
+}
